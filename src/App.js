16eec27b3ee2e301b5fc88076b4434e7c76ac947
camilla <camilla.gausland@gmail.com>
2020-04-24 12:02:19 +0200

    design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Card } from './lib/Card'
+import styled from 'styled-components/macro'
+
+const ThinnerCard = styled(Card) `
+  width: 400px;
+`
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <ThinnerCard 
+        coverImage='https://www.fillmurray.com/500/300'
+        thumbnailUrl='https://www.fillmurray.com/100/100'
+        title='This is the title' 
+        secondaryText='Secondary text' />
+
+        <Card title='Only a title' secondaryText='and a second text' />
+
+        <ThinnerCard>
+          <h1>Hello from children</h1> 
+        </ThinnerCard>
     </div>
   )
 }
